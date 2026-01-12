a13ce2a56e7e10275c5d9c4688c679eeae115cf0
Daniel Duque <danielvduque@gmail.com>
2026-01-12 20:39:07 +0100

    feat: add icon, remove old files

--- downloadImage.js
+++
@@ -1,6 +0,0 @@
-setTimeout(() => { 
-    var imageClass = document.getElementsByClassName("x5yr21d xu96u03 x10l6tqk x13vifvy x87ps6o xh8yej3")
-    console.log(imageClass);
-    console.log(imageClass[0]);
-    console.log(imageClass[0].src);
-}, 5000);
